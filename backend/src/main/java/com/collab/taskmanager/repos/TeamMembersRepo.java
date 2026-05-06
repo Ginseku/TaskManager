aa1c8e11f3ffbf19cb14fca7ee1a4f325f4f30fb
mykyta <380630700519b@gmail.com>
2026-05-06 19:06:59 +0200

    Added DTO and change Repos

--- backend/src/main/java/com/collab/taskmanager/repos/TeamMembersRepo.java
+++ backend/src/main/java/com/collab/taskmanager/repos/TeamMembersRepo.java
@@ -1,9 +1,14 @@
 package com.collab.taskmanager.repos;
 
 
-import com.collab.taskmanager.entities.Team;
 import com.collab.taskmanager.entities.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TeamMembersRepo extends JpaRepository<TeamMember, Long> {
+    boolean existsByTeamIdAndMemberId(Long teamId, Long currentUserId);
+    Optional<TeamMember> findByTeamIdAndMemberId(Long teamId, Long currentUserId);
+    List<TeamMember> findByMemberId(Long memberId);
 }
--- backend/src/main/java/com/collab/taskmanager/repos/TeamRepo.java
+++ backend/src/main/java/com/collab/taskmanager/repos/TeamRepo.java
@@ -3,5 +3,8 @@ package com.collab.taskmanager.repos;
 import com.collab.taskmanager.entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepo extends JpaRepository<Team, Long> {
+    boolean existsByCreatedBy(Long id);
 }
