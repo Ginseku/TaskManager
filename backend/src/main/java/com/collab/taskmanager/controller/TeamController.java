dc3fd954d56e7741102e939589467622b18e7e06
mykyta <380630700519b@gmail.com>
2026-05-06 19:17:09 +0200

    Add logic Create team Get all teams Get team by id Add member into team

--- backend/src/main/java/com/collab/taskmanager/controller/TeamController.java
+++ backend/src/main/java/com/collab/taskmanager/controller/TeamController.java
@@ -1,16 +1,18 @@
 package com.collab.taskmanager.controller;
 
-import com.collab.taskmanager.entities.User;
+import com.collab.taskmanager.dto.request.AddMemberRequest;
+import com.collab.taskmanager.dto.request.CreateTeamRequest;
+import com.collab.taskmanager.dto.response.TeamResponse;
 import com.collab.taskmanager.entities.UserPrincipal;
 import com.collab.taskmanager.service.TeamService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/team")
+@RequestMapping("/teams")
 public class TeamController {
 
     private final TeamService teamService;
@@ -20,9 +22,27 @@ public class TeamController {
     }
 
     @PostMapping("/createTeam")
-    public ResponseEntity<String> createTeam(@AuthenticationPrincipal UserPrincipal userDetail){
-        return teamService.createTeam(userDetail.getUser());
+    public ResponseEntity<TeamResponse> createTeam(@AuthenticationPrincipal UserPrincipal userDetail, @RequestBody CreateTeamRequest request){
+        return teamService.createTeam(userDetail.getUser(), request);
     }
 
+    @GetMapping
+    public List<TeamResponse> getMyTeams(@AuthenticationPrincipal UserPrincipal user) {
+        return teamService.getUserTeams(user);
+    }
+
+    @GetMapping("/{teamId}")
+    public TeamResponse getTeam(@PathVariable Long teamId) {
+        return teamService.getTeam(teamId);
+    }
+
+    @PostMapping("/{teamId}/members")
+    public ResponseEntity<String> addMember(
+            @AuthenticationPrincipal UserPrincipal userDetails,
+            @PathVariable Long teamId,
+            @RequestBody AddMemberRequest request){
+        teamService.addMember(userDetails,request.userId(),teamId);
+        return ResponseEntity.ok("User added");
+    }
 
 }
