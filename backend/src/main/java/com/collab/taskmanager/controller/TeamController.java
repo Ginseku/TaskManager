7357e115b5ecb0e8a29d1aa2cddd025b05fb01c8
mykyta <380630700519b@gmail.com>
2026-05-08 16:27:36 +0200

    delete unused imports

--- backend/src/main/java/com/collab/taskmanager/controller/TeamController.java
+++ backend/src/main/java/com/collab/taskmanager/controller/TeamController.java
@@ -9,7 +9,6 @@ import com.collab.taskmanager.service.TeamService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
