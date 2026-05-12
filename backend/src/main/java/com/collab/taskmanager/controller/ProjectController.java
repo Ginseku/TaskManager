fb861a9ffccb35c99aa5ec0dd6d5bce94028e8db
mykyta <380630700519b@gmail.com>
2026-05-12 17:55:56 +0200

    Added OpenApi

--- backend/src/main/java/com/collab/taskmanager/controller/ProjectController.java
+++ backend/src/main/java/com/collab/taskmanager/controller/ProjectController.java
@@ -3,6 +3,9 @@ package com.collab.taskmanager.controller;
 import com.collab.taskmanager.dto.response.GetProjectMembersNameResponse;
 import com.collab.taskmanager.entities.UserPrincipal;
 import com.collab.taskmanager.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,12 +22,33 @@ public class ProjectController {
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
     }
-
+    @Operation(
+            summary = "Create project",
+            description = "Create project by team Owner"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Project created successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Access denied"),
+            @ApiResponse(responseCode = "404", description = "User or team not found"),
+            @ApiResponse(responseCode = "409", description = "User is not Team member")
+    })
     @PostMapping("/{teamId}/")
     public void createProject(@PathVariable("teamId") Long teamId, @AuthenticationPrincipal UserPrincipal userPrincipal){
         projectService.createProject(teamId,userPrincipal);
     }
 
+    @Operation(
+            summary = "Get members",
+            description = "Get all members who has access to the project"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Project created successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Access denied"),
+            @ApiResponse(responseCode = "404", description = "Project not found"),
+            @ApiResponse(responseCode = "409", description = "User is not Team member")
+    })
     @GetMapping("/{projectId}/")
     public ResponseEntity<List<GetProjectMembersNameResponse>> getProjectMembers(@PathVariable Long projectId, @AuthenticationPrincipal UserPrincipal currentUser){
         return ResponseEntity.ok(projectService.getProjectMembers(projectId, currentUser));
